b4995df514f7060e34d7aaf62ff841c065ddd116
danthogi <srivani.danthogi@gmail.com>
2021-04-12 23:39:07 +0530

    added platform

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1,platform;
 var backgroundImg;
 
 function preload() {
@@ -17,6 +17,8 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    platform = new Ground(800,height,1200,20)
+
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -59,4 +61,6 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
+
 }
